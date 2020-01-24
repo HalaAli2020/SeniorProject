4aeeb32d886e8a197b3a66f0bd4883a11148f1a0
Tag1320 <gl0132@wayne.edu>
2020-01-24 18:17:16 -0500

    connected to firebase

--- app/src/main/java/com/example/seniorproject/LoginActivity.kt
+++ app/src/main/java/com/example/seniorproject/LoginActivity.kt
@@ -8,7 +8,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login)
 
 
     }
