5200fdeda276f695452bd4d7b18972762ebb5c4c
Tag1320 <gl0132@wayne.edu>
2020-01-27 18:25:11 -0500

    Updated post model constructor added

--- app/src/main/java/com/example/seniorproject/model/Posts.kt
+++ app/src/main/java/com/example/seniorproject/model/Posts.kt
@@ -6,46 +6,24 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class Posts()
+class Posts(private val txt: String?,private val courseID: Int?, private val uid: String?)
 {
     private val database = FirebaseDatabase.getInstance()
-    private var txt: String
-    private var courseID : Int
-    private var commentID : Int?
     private var ptime: Long
-    private var uid : String?
     init {
         // backend will initalize values here or set to null if
-        txt = "A"
-        courseID = 1
-        commentID = null
         ptime = 1
-        ptime = 1
-        uid = "1"
-    }
-
-    fun newPost(t: String, CourseID : Int, ID : String?)
-    {
-        txt = t
-        ptime = 1
-        uid = ID
-        courseID = CourseID
-
 
     }
 
-    fun gettxt() : String
+
+
+    fun gettxt() : String?
     {
         return txt
     }
-    fun getcousreID() : Int
-    {
-        return courseID
-    }
-    fun getcommentID() : Int?
-    {
-        return commentID
-    }
+   
+
     fun getPtime(): Long
     {
         return ptime
