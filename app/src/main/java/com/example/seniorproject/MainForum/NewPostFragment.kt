398c1c1022003922aac586450ca921ccc42ccf51
halaali <aliha2016@gmail.com>
2020-02-12 16:51:54 -0500

    completed dagger dependency injection on new post. Palak and I worked together on this.

--- app/src/main/java/com/example/seniorproject/MainForum/NewPostFragment.kt
+++ app/src/main/java/com/example/seniorproject/MainForum/NewPostFragment.kt
@@ -7,31 +7,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import com.example.seniorproject.InjectorUtils
+import androidx.lifecycle.ViewModelProvider
+//import com.example.seniorproject.InjectorUtils
 import com.example.seniorproject.databinding.NewPostFragmentBinding
-
+import com.example.seniorproject.Dagger.DaggerAppComponent
 import com.example.seniorproject.R
 import com.example.seniorproject.databinding.FragmentHomeBinding
 import com.example.seniorproject.viewModels.HomeFragmentViewModel
 import com.example.seniorproject.viewModels.NewPostFragmentViewModel
+import javax.inject.Inject
 
 class NewPostFragment : Fragment() {
+
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
+    lateinit var viewModel: NewPostFragmentViewModel
 
     companion object {
         fun newInstance() = NewPostFragment()
     }
 
-    private lateinit var viewModel: NewPostFragmentViewModel
+    //private lateinit var viewModel: NewPostFragmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val factory = InjectorUtils.provideNewPostViewModelFactory()
+        DaggerAppComponent.create().inject(this)
         val binding: NewPostFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.new_post_fragment, container, false)
         viewModel = ViewModelProviders.of(this, factory).get(NewPostFragmentViewModel::class.java)
         val view = inflater.inflate(R.layout.new_post_fragment, container, false)
+
+        /*val factory = InjectorUtils.provideNewPostViewModelFactory()
+        val binding: NewPostFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.new_post_fragment, container, false)
+        viewModel = ViewModelProviders.of(this, factory).get(NewPostFragmentViewModel::class.java)
+        val view = inflater.inflate(R.layout.new_post_fragment, container, false)*/
 
         binding.newPostViewModel = viewModel
         binding.lifecycleOwner = this
