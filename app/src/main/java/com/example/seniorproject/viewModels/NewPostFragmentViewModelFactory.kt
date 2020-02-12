3e5789dc995703e1adc880aef44fb1b340c8dac0
halaali <aliha2016@gmail.com>
2020-02-12 16:53:44 -0500

    commented out this factory because we don't need it. I did not delete because I want to explain to my team what steps I took for dagger multibinding.

--- app/src/main/java/com/example/seniorproject/viewModels/NewPostFragmentViewModelFactory.kt
+++ app/src/main/java/com/example/seniorproject/viewModels/NewPostFragmentViewModelFactory.kt
@@ -4,11 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.seniorproject.data.repositories.PostRepository
 
-@Suppress("UNCHECKED_CAST")
+/*@Suppress("UNCHECKED_CAST")
 class NewPostFragmentViewModelFactory(private val repository: PostRepository) :
     ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return NewPostFragmentViewModel(repository) as T
     }
-}
+}*/
