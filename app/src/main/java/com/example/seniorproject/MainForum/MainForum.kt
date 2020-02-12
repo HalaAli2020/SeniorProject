c39009cf6a794b1e0f7969e35ed2e9cb0feca11e
palak <palakmpatel888@gmail.com>
2020-02-12 15:04:01 -0500

    small change to make another pull request to redo reversion

--- app/src/main/java/com/example/seniorproject/MainForum/MainForum.kt
+++ app/src/main/java/com/example/seniorproject/MainForum/MainForum.kt
@@ -100,6 +100,7 @@ class MainForum : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
+            //comment so commit will work
         }
     }
 
