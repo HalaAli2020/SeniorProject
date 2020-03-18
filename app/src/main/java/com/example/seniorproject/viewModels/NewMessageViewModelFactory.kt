6c3721dc90028f69940118409b20e8d0819f3133
Tag1320 <Gzelrt44!>
2020-03-18 15:01:48 -0400

    Merged with current master 3/18

---
+++ app/src/main/java/com/example/seniorproject/viewModels/NewMessageViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.seniorproject.viewModels.Factories
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.seniorproject.data.repositories.MessagesRepo
+import com.example.seniorproject.viewModels.NewMessageViewModel
+
+@Suppress("UNCHECKED_CAST")
+class NewMessageViewModelFactory(private val repository: MessagesRepo) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return NewMessageViewModel(repository) as T
+    }
+}
