686567fcc225c001992929a802f7312ab440b510
palak <palakmpatel888@gmail.com>
2020-04-02 01:57:27 -0400

    uncommented constructor

--- app/src/main/java/com/example/seniorproject/data/models/ChatMessage.kt
+++ app/src/main/java/com/example/seniorproject/data/models/ChatMessage.kt
@@ -1,5 +1,5 @@
 package com.example.seniorproject.data.models
 
 data class ChatMessage(val id: String, var text: String?, val fromID: String, val toId: String, val timestamp: Long){
-    //constructor(): this("","","","",-1)
+    constructor(): this("","","","",-1)
 }
