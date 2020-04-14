085743286db1e26b7bf6d14934a61bd4b8b1db09
palak <palakmpatel888@gmail.com>
2020-04-14 01:40:06 -0400

    added a callback to unblock user to fix block from comments bug

--- app/src/main/java/com/example/seniorproject/viewModels/SettingsViewModel.kt
+++ app/src/main/java/com/example/seniorproject/viewModels/SettingsViewModel.kt
@@ -2,10 +2,13 @@ package com.example.seniorproject.viewModels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.seniorproject.Utils.EmailCallback
 import com.example.seniorproject.data.Firebase.FirebaseData
+import com.example.seniorproject.data.interfaces.FirebaseCallbackItem
 import com.example.seniorproject.data.interfaces.FirebaseCallbackUserListFlow
 import com.example.seniorproject.data.models.Post
 import com.example.seniorproject.data.repositories.PostRepository
+import com.google.firebase.database.DataSnapshot
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
@@ -23,7 +26,22 @@ class SettingsViewModel @Inject constructor(private val repository: PostReposito
             }
         })
     }
-    fun unblockUser(username : String ) = repository.unblockUser(username)
+
+    fun unblockUser(username : String , callback : EmailCallback) = repository.unblockUser(username, object : FirebaseCallbackItem {
+        override fun onStart() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onFailure() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onMessage(data: DataSnapshot) {
+            callback.getEmail("done")
+        }
+
+    })
+
 }
 
 interface BlockedUserListCallback{
